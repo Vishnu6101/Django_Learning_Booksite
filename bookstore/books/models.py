121a433856366251f7845099c896c57cee3215ec
Vishnukumar NV <vishnukumarnv6101@gmail.com>
2022-02-13 14:06:38 +0530

    generic views, template inheritance done

--- bookstore/books/models.py
+++ bookstore/books/models.py
@@ -1,19 +1,23 @@
-from tkinter.tix import Tree
 from django.db import models
 
 # Create your models here.
 
+class Author(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
 
 class Book(models.Model):
     title = models.CharField(max_length=256, null=True)
     pageCount = models.IntegerField(default=0)
     thumbnailUrl = models.CharField(max_length=256, null=True)
-    shortDescription = models.CharField(max_length=256, null=True)
+    shortDescription = models.CharField(max_length=1024, null=True)
     longDescription = models.TextField(null=True)
-    authors = models.CharField(max_length=256, null=True)
     category = models.CharField(max_length=256, null=True)
     publishedOn = models.DateTimeField(null=True)
     isbn = models.CharField(max_length=256, null=True)
+    authors = models.ManyToManyField(Author)
 
     def __str__(self) -> str:
         return f"{self.id}. {self.title}"
